750437c881983df24f56910df3d9fe5c5faa851a
Akash Hegde <55193167+akashhegde2012@users.noreply.github.com>
2025-12-06 07:10:11 +0000

    database

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import sqlite3, datetime
+
 
 app = Flask(__name__)
 
@@ -10,11 +12,33 @@ def index():
 def main():
     username = request.form.get("username")
     print(username)
+    timestamp = datetime.datetime.now()
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute("Insert into user(name, timestamp) values(?,?)", (username, timestamp))
+    conn.commit()
+    conn.close()
     return(render_template("main.html"))
 
 @app.route("/paynow", methods=["GET", "POST"])
 def paynow():
     return(render_template("paynow.html"))
+
+@app.route("/userlog", methods=["GET", "POST"])
+def userlog():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute("SELECt * from USER")
+    r = ""
+    for row in c:
+        r = r+str(row)
+    print(r)
+    conn.close()
+    return(render_template("userlog.html", r=r))
+
+@app.route("/deleteuserlog", methods=["GET", "POST"])
+def deleteuserlog():
+    return(render_template("deleteuserlog.html"))
 
 if __name__ == "__main__":
     app.run()
